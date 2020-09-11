18d4376180d8f84b112e366b87c734b247edd8e1
Margot <margot.ginestetropini@gmail.com>
2020-09-11 14:14:19 +0200

    add of gravity

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const img = new Image(); //J'appelle mon set d'image
 img.src = "../media/flappy-bird-set.png";
 
 //General settings
-let generalPlaying = false;
+let gamePlaying = false;
 const gravity = 0.5;
 const speed = 6.2;
 const size = [51, 36]; //taille oiseau
@@ -35,18 +35,50 @@ const render = () => {
     canvas.height
   );
 
+  //Superposition des images de fond
   context.drawImage(
-    img, // dans img
-    432, // prend l'oiseau qui commence à 432 sur l'axe des x
-    Math.floor((index % 9) / 3) * size[1], //sur l'axe des Y math floor de index, on divise par 3 X la taille en hauteur de l'oiseau. En gros les 3 images d'oiseau se mettent les unes sur les autres et on dirait qu'il bat des ailes
-    ...size, // const
-    canvas.width / 2 - size[0] / 2, // la largeur du canvas /2 - la taille de la largeur de l'oiseau
-    flyHeight, // const
-    ...size // const
+    img,
+    0, // tu commences a 0 sur les X
+    0, // tu commences a 0 sur les Y
+    canvas.width, // tu vas jusqu'a canvas.width
+    canvas.height, // tu vas jusqu'à canvas.height
+    -((index * (speed / 2)) % canvas.width), // en enlevant le +canvas.width les images se suivent correctement
+    0,
+    canvas.width,
+    canvas.height
   );
 
-  flyHeight = canvas.height / 2 - size[1] / 2; // la hauteur de vol : on prend la hauteur du  canvas on divise par 2, on prend la taille de l'oiseau et on  divise par 2
+  if (gamePlaying) {
+    context.drawImage(
+      img, // dans img
+      432, // prend l'oiseau qui commence à 432 sur l'axe des x
+      Math.floor((index % 9) / 3) * size[1], //sur l'axe des Y math floor de index, on divise par 3 X la taille en hauteur de l'oiseau. En gros les 3 images d'oiseau se mettent les unes sur les autres et on dirait qu'il bat des ailes
+      ...size,
+      cTenth, // On le position a un dixième de l'écran sur l'axe des X
+      flyHeight,
+      ...size
+    );
+    flight += gravity; // Permet à mon oiseau de  pas sorti de l'écran
+    flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]); // Tu me prend le plus petit entre : flyHeight + flight et  ce total canvas.height. du coup l'oiseau aura tendance à descendre !!
+  } else {
+    context.drawImage(
+      img, // dans img
+      432, // prend l'oiseau qui commence à 432 sur l'axe des x
+      Math.floor((index % 9) / 3) * size[1], //sur l'axe des Y math floor de index, on divise par 3 X la taille en hauteur de l'oiseau. En gros les 3 images d'oiseau se mettent les unes sur les autres et on dirait qu'il bat des ailes
+      ...size, // const
+      canvas.width / 2 - size[0] / 2, // la largeur du canvas /2 - la taille de la largeur de l'oiseau
+      flyHeight, // const
+      ...size // const
+    );
+    flyHeight = canvas.height / 2 - size[1] / 2; // la hauteur de vol : on prend la hauteur du  canvas on divise par 2, on prend la taille de l'oiseau et on  divise par 2
 
+    context.fillText(`Meilleur score : ${bestScore}`, 55, 245); //Me permet d'écrire dans le canvas. 55 axe des X en px/ 245 axe des Y en px
+    context.fillText(`Cliquez pour jouer`, 48, 535); //48 axe X 543 axe Y
+    context.font = "bold 30px courier";
+  }
   window.requestAnimationFrame(render); // on fait tourner en boucle
 };
+
 img.onload = render; // au chargement de l'image, tu peux commencer à lancer render
+document.addEventListener("click", () => (gamePlaying = true)); // Au clique, tu changes le booléen de gamePlaying
+window.onclick = () => (flight = jump); // on lui donne la valeur du jump
